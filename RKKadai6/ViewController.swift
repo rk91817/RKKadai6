4990dbdcdf11bd24515983c6d74f2cd70ffc2973
akio0911 <akio0911@gmail.com>
2022-06-29 17:38:10 +0900

    feedback

--- RKKadai6/ViewController.swift
+++ RKKadai6/ViewController.swift
@@ -10,35 +10,39 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet private weak var randomIntLabel: UILabel!
     @IBOutlet private weak var slider: UISlider!
-    
+
+    private var correctAnswer = 0
+
     @IBAction func didTapButton(_ sender: Any) {
         let sliderValue: Int = Int(slider.value)
-        let randomInt = Int(randomIntLabel.text ?? "0")
-        if sliderValue == randomInt {
+        if sliderValue == correctAnswer {
             presentAlert(message: "あたり！\nあなたの値: \(sliderValue)")
         } else {
             presentAlert(message: "はずれ！\nあなたの値: \(sliderValue)")
         }
     }
-    
+
     private func randomNum () -> Int {
-        let num = Int(1 + arc4random_uniform(100))
-        return num
+        Int.random(in: 1...100)
     }
-    
+
     private func presentAlert(message: String) {
-          let alert = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "再挑戦", style: .default, handler: {_ in
-            self.randomIntLabel.text = String(self.randomNum())
-            self.slider.value = 50
+        let alert = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "再挑戦", style: .default, handler: { _ in
+            self.resetGame()
         })
-          alert.addAction(action)
-          present(alert, animated: true, completion: nil)
-      }
-    
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        resetGame()
+    }
+
+    private func resetGame() {
+        correctAnswer = randomNum()
+        randomIntLabel.text = String(correctAnswer)
         slider.value = 50
-        randomIntLabel.text = String(randomNum())
     }
 }
